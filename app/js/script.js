62486ea759f39e6bbaed8e1580509a5e70769fa9
AnnLP2 <ann.vitalina@gmail.com>
2023-01-16 20:59:57 +0200

    added menu js

--- app/js/script.js
+++ app/js/script.js
@@ -1,22 +1,46 @@
-$(".js-slider").slick({
-    arrows: false,
-    dots: true,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 10,
-    dotsClass: "my-dots",
-    mobileFirst: true,
-    responsive: [
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-                dots: true,
+(function () {
+    $(".js-slider").slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 10,
+        dotsClass: "my-dots",
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    dots: true,
+                },
             },
-        },
-    ],
-});
+        ],
+    });
+})();
+
+(function () {
+    const burger = document.querySelector(".menu__burger");
+    if (!burger) return;
+    const menu = document.querySelector(".menu__list");
+    const overlay = document.querySelector(".menu__overlay");
+    const body = document.querySelector("body");
+
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("show");
+        burger.classList.toggle("show");
+        overlay.classList.toggle("show");
+        body.classList.toggle("stop-scroll");
+    });
+
+    overlay.addEventListener("click", function () {
+        menu.classList.remove("show");
+        burger.classList.remove("show");
+        overlay.classList.remove("show");
+        body.classList.remove("stop-scroll");
+    });
+})();
