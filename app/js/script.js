c411ba0499d97c2845b766374dcf87d9afbd1a64
AnnLP2 <ann.vitalina@gmail.com>
2023-01-19 17:46:53 +0200

    added homepage team slider

--- app/js/script.js
+++ app/js/script.js
@@ -62,3 +62,28 @@
         });
     });
 })();
+
+(function () {
+    $(".js-slider-team").slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 10,
+        dotsClass: "my-dots",
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                },
+            },
+        ],
+    });
+})();
